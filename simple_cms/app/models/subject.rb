09f140922fce8aab76c624d0547c55e5dbb7a712
Kevin Skoglund <kevin@novafabrica.com>
2020-08-14 21:15:34 -0400

    Finish movie 03_02

--- simple_cms/app/models/subject.rb
+++ simple_cms/app/models/subject.rb
@@ -15,4 +15,23 @@ class Subject < ApplicationRecord
   validates_presence_of :position
   validates_numericality_of :position, :greater_than => 0
 
+  after_create :log_create
+  after_update :log_update
+  after_save :log_save
+
+
+  private
+
+    def log_create
+      logger.info("Subject ID #{id} was created")
+    end
+
+    def log_update
+      logger.info("Subject ID #{id} was updated")
+    end
+
+    def log_save
+      logger.info("Subject ID #{id} was saved")
+    end
+
 end
